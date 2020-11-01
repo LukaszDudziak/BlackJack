28e11b6757d3b103fc54576577a6aa395085a77e
dudziakpudziak <lukaszdudziak777@gmail.com>
2020-11-01 16:54:45 +0100

    final_1.0

--- app/Game.js
+++ app/Game.js
@@ -1,29 +1,88 @@
 import { Deck } from './Deck.js'
+import { Message } from './Message.js';
 import { Player } from './Player.js'
 import { Table } from './Table.js';
 
 class Game{
-    constructor({ player, table}){
+    constructor({ player, table, dealerPoints, playerPoints, hitButton, standButton, messageBox}){
+        this.hitButton = hitButton;
+        this.standButton = standButton;
+        this.dealerPoints = dealerPoints;
+        this.playerPoints = playerPoints;
+        this.messageBox = messageBox;
         this.player = player;
         this.dealer = new Player('Krupier');
-        this.table = table
+        this.table = table;
         this.deck = new Deck();
         this.deck.shuffle();
     }
     
     run(){
-       this.dealCards();
+        this.hitButton.addEventListener('click',(event)=> this.hitCard());
+        this.standButton.addEventListener('click',(event)=> this.dealerPlays());
+        this.dealCards();
+    }
+
+    hitCard(){
+        const card = this.deck.pickOne();
+        this.player.hand.addCard(card);
+        this.table.showPlayersCard(card);
+        this.playerPoints.innerHTML = this.player.calculatePoints();
     }
 
     dealCards(){
         for(let n = 0; n < 2; n++){
             let card1 = this.deck.pickOne();
             this.player.hand.addCard(card1);
-            this.table.showPlayersCard(card1.render())
+            this.table.showPlayersCard(card1)
             
             let card2 = this.deck.pickOne();
             this.dealer.hand.addCard(card2);
-            this.table.showDealersCard(card2.render())
+            this.table.showDealersCard(card2)
+        }
+        this.playerPoints.innerHTML = this.player.calculatePoints();
+        this.dealerPoints.innerHTML = this.dealer.calculatePoints();
+    }
+
+    dealerPlays(){
+        while( this.dealer.points <= this.player.points && this.dealer.points <= 21 && this.player.points <= 21){
+            const card = this.deck.pickOne();
+            this.dealer.hand.addCard(card);
+            this.table.showDealersCard(card);
+            this.dealerPoints.innerHTML = this.dealer.calculatePoints();
+        }
+
+        this.endTheGame();
+    }
+
+    endTheGame(){
+        this.hitButton.removeEventListener('click', (event) => this.hitCard());
+        this.standButton.removeEventListener('click', (event) => this.dealerPlays());
+
+        this.hitButton.style.display = 'none';
+        this.standButton.style.display = 'none';
+
+        if(this.playerPoints < 21 && this.player.points == this.dealer.points){
+            this.messageBox.setText('remis').show();
+            return;
+        }
+
+        if(this.player.points > 21){
+            this.messageBox.setText('wygrał krupier').show();
+
+            return
+        }
+
+        if(this.dealer.points > 21){
+            this.messageBox.setText('wygrał gracz').show();
+
+            return
+        }
+
+        if(this.player.points < this.dealer.points){
+            this.messageBox.setText('wygrał krupier').show();
+
+            return
         }
     }
 }
@@ -32,11 +91,19 @@ const table = new Table(
     document.getElementById('dealersCards'),
     document.getElementById('playersCards')
 );
+
+const messageBox = new Message(document.getElementById('message'));
+
 const player = new Player('Lu');
 //utworzenie nowej gry z propertami
 const game = new Game({
+    hitButton: document.getElementById('hit'),
+    standButton: document.getElementById('stand'),
+    dealerPoints: document.getElementById('dealerPoints'),
+    playerPoints: document.getElementById('playerPoints'),
     player,
-    table
+    table,
+    messageBox
 })
 
 game.run();
